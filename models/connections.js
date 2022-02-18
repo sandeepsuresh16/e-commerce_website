efb82aad72e391d19185c9f7b2e0a570f1a941af
Sandeep <69909315+sandeepsuresh16@users.noreply.github.com>
2022-02-18 15:39:39 +0530

    Update connections.js

--- models/connections.js
+++ models/connections.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://user:user@cluster0.pgmou.mongodb.net/project1',{
+mongoose.connect('mongodb+srv://cluster0.pgmou.mongodb.net/project1',{
     useNewUrlParser:true,
     useUnifiedTopology:true
 }).then(() =>{console.log('connected to DB(project1) successfully')})
